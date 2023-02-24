f23490e90a5fb89fd70ac0a386d43136116978d4
Allan Zhou <allanlinzhou@gmail.com>
2023-02-24 09:43:40 -0500

    Drivetrain code edits

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -10,6 +12,16 @@ public class Drivetrain extends SubsystemBase {
     private CANSparkMax leftMaster; 
     private CANSparkMax rightFollower; 
     private CANSparkMax leftFollower; 
+
+    private SparkMaxPIDController rightController; 
+    private SparkMaxPIDController leftController;
+
+    private static double kP = 0.1; 
+    private static double kI = 0.0; 
+    private static double kD = 0.0; 
+    
+    private RelativeEncoder rightEncoder; 
+    private RelativeEncoder leftEncoder; 
 
     public Drivetrain(int rightMaster, int leftMaster, int rightFollower, int leftFollower) {
         this.rightMaster = new CANSparkMax(rightMaster, MotorType.kBrushless); 
@@ -21,6 +33,22 @@ public class Drivetrain extends SubsystemBase {
         this.leftFollower.follow(this.leftMaster); 
 
         this.rightMaster.setInverted(true);
+
+        this.rightController = this.rightMaster.getPIDController();
+        this.leftController = this.leftMaster.getPIDController(); 
+
+        this.rightController.setP(kP); 
+        this.rightController.setI(kI); 
+        this.rightController.setD(kD); 
+        this.rightController.setOutputRange(-1.0, 1.0);
+
+        this.leftController.setP(kP); 
+        this.leftController.setI(kI); 
+        this.leftController.setD(kD); 
+        this.leftController.setOutputRange(-1.0, 1.0); 
+
+        this.rightEncoder = this.rightMaster.getEncoder(); 
+        this.leftEncoder = this.leftMaster.getEncoder(); 
     }
 
     public void arcadeDrive(double translation, double rotation, double scalingFactor) {
