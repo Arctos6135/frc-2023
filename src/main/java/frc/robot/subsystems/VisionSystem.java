69909478865d876b6ce6c2d7a9a6182e609dd468
Allan Zhou <allanlinzhou@gmail.com>
2023-02-27 11:38:49 -0500

    Added Vision System Methods

--- src/main/java/frc/robot/subsystems/VisionSystem.java
+++ src/main/java/frc/robot/subsystems/VisionSystem.java
@@ -1,6 +1,12 @@
 package frc.robot.subsystems;
 
 import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonUtils;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+import edu.wpi.first.math.util.Units;
+import frc.robot.constants.VisionConstants;
 
 public class VisionSystem {
     // Front Vision (Arm, Placing Game Pieces)
@@ -10,8 +16,40 @@ public class VisionSystem {
     private PhotonCamera backCamera; 
 
     public VisionSystem() {
-        limelight = new PhotonCamera(null, "Limelight"); 
-        backCamera = new PhotonCamera(null, "Back Camera"); 
-        
+        this.limelight = new PhotonCamera("Limelight"); 
+        this.backCamera = new PhotonCamera("Back Camera"); 
+    }
+
+    public PhotonPipelineResult getLimelightResult() {
+        return this.limelight.getLatestResult(); 
+    }
+
+    public PhotonPipelineResult getBackCameraResult() {
+        return this.backCamera.getLatestResult();
+    }
+
+    public boolean limelightTargets() {
+        return getLimelightResult().hasTargets(); 
+    }
+
+    public boolean backCameraTargets() {
+        return getBackCameraResult().hasTargets(); 
+    }
+
+    public PhotonTrackedTarget getLimelightTarget() {
+        return getLimelightResult().getBestTarget();
+    }
+    
+    public PhotonTrackedTarget getBackCameraTarget() {
+        return getLimelightResult().getBestTarget(); 
+    }
+
+    public double calculateRange(PhotonCamera camera) {
+        return PhotonUtils.calculateDistanceToTargetMeters(
+            VisionConstants.CAMERA_HEIGHT, 
+            VisionConstants.TARGET_HEIGHT, 
+            VisionConstants.CAMERA_PITCH_RADIANS, 
+            Units.degreesToRadians(camera.getLatestResult().getBestTarget().getPitch())
+        ); 
     }
 }
