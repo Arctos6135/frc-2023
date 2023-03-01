38d2bbbc7317e4ced124a858e1a275e933e6bc59
MrDankinator <ayaan.adrito@gmail.com>
2023-03-01 16:00:23 -0500

    Push elevator joystick control

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,13 +1,10 @@
 package frc.robot.subsystems;
 
 import frc.robot.constants.ElevatorConstants;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-
-import frc.robot.constants.DriveConstants;
 
 public class Elevator extends SubsystemBase {
     public final TalonSRX elevatorMotor;
