db581688e9d5daf4fdfc57d1376af1c4f8c572d9
Nick Armstrong <75282394+n-arms@users.noreply.github.com>
2023-03-07 09:09:37 -0500

    Add code to turn the robot a specified number of degrees

--- src/main/java/frc/robot/commands/auto/MobilityDockWide.java
+++ src/main/java/frc/robot/commands/auto/MobilityDockWide.java
@@ -2,7 +2,7 @@ package frc.robot.commands.auto;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.commands.driving.DriveForward;
-import frc.robot.commands.driving.TurnLeft90;
+import frc.robot.commands.driving.Turn;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Claw;
 import frc.robot.subsystems.Drivetrain;
@@ -12,13 +12,13 @@ public class MobilityDockWide {
     public static Command getCommand(Drivetrain drivetrain, Elevator elevator, Arm arm, Claw claw) {
         Command sequence = new DriveForward(-0.2, 0.5, drivetrain)
             .andThen(new DriveForward(0.2, 1, drivetrain))
-            .andThen(new TurnLeft90(drivetrain))
+            .andThen(new Turn(drivetrain, 45))
             .andThen(new DriveForward(0.2, 1, drivetrain))
-            .andThen(new TurnLeft90(drivetrain))
+            .andThen(new Turn(drivetrain, -45))
             .andThen(new DriveForward(0.2, 1, drivetrain))
-            .andThen(new TurnLeft90(drivetrain))
+            .andThen(new Turn(drivetrain, -45))
             .andThen(new DriveForward(0.2, 1, drivetrain))
-            .andThen(new TurnLeft90(drivetrain));
+            .andThen(new Turn(drivetrain, 45));
 
         return sequence;
     }
