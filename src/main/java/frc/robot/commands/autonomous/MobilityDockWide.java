3714ef9b0519e105d17fa1f51b69559c4351eb33
n-arms <75282394+n-arms@users.noreply.github.com>
2023-03-10 08:05:42 -0500

    Write code to prep for final day of drivetrain testing

--- src/main/java/frc/robot/commands/autonomous/MobilityDockWide.java
+++ src/main/java/frc/robot/commands/autonomous/MobilityDockWide.java
@@ -2,18 +2,28 @@ package frc.robot.commands.autonomous;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.commands.driving.DriveForward;
+import frc.robot.commands.driving.DriveForwardEncoded;
 import frc.robot.commands.driving.Turn;
+import frc.robot.commands.driving.TurnEncoded;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Claw;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Elevator;
 
 public class MobilityDockWide {
-    public static Command getCommand(Drivetrain drivetrain, Elevator elevator, Arm arm, Claw claw) {
+    /*public static Command getCommand(Drivetrain drivetrain, Elevator elevator, Arm arm, Claw claw) {
         Command sequence = new DriveForward(-0.2, 1, drivetrain)
             .andThen(new DriveForward(0.2, 2, drivetrain))
             .andThen(new Turn(drivetrain, 1, -0.2))
             .andThen(new DriveForward(0.2, 4, drivetrain));
+
+        return sequence;
+    }*/
+    public static Command getCommand(Drivetrain drivetrain, Elevator elevator, Arm arm, Claw claw) {
+        Command sequence = new DriveForwardEncoded(-0.2, 0.5, drivetrain)
+            .andThen(new DriveForwardEncoded(0.2, 1, drivetrain))
+            .andThen(new TurnEncoded(drivetrain, 20, -0.2))
+            .andThen(new DriveForwardEncoded(0.2, 4, drivetrain));
 
         return sequence;
     }
