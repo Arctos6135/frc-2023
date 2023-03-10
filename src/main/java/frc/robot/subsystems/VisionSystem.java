d42279bcb1fb753126030d9a29e6d9885ccbc611
Gabriel Dinner-David <gabydinnerdavid@gmail.com>
2023-03-10 16:04:19 -0500

    remove photonvision for claw camera

--- src/main/java/frc/robot/subsystems/VisionSystem.java
+++ src/main/java/frc/robot/subsystems/VisionSystem.java
@@ -13,38 +13,22 @@ public class VisionSystem {
     private PhotonCamera limelight; 
     public static final String LIMELIGHT_URL = "http://photonvision.local:5800/"; 
 
-    // Back Vision (April Tag)
-    private PhotonCamera clawCamera; 
-
     public VisionSystem() {
         this.limelight = new PhotonCamera("Limelight"); 
-        this.clawCamera = new PhotonCamera("Claw Camera"); 
     }
 
     public PhotonPipelineResult getLimelightResult() {
         return this.limelight.getLatestResult(); 
     }
 
-    public PhotonPipelineResult getBackCameraResult() {
-        return this.clawCamera.getLatestResult();
-    }
-
     public boolean limelightTargets() {
         return getLimelightResult().hasTargets(); 
-    }
-
-    public boolean backCameraTargets() {
-        return getBackCameraResult().hasTargets(); 
     }
 
     public PhotonTrackedTarget getLimelightTarget() {
         return getLimelightResult().getBestTarget();
     }
     
-    public PhotonTrackedTarget getBackCameraTarget() {
-        return getLimelightResult().getBestTarget(); 
-    }
-
     public double calculateLimelightRange(boolean tape) {
         return PhotonUtils.calculateDistanceToTargetMeters(
             VisionConstants.CAMERA_HEIGHT_METERS, 
