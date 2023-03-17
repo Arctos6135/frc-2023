eec37b8ce2bb609047adbe5192722c02ea134d22
Zachary Runge Cormack <cormack.zachary@gmail.com>
2023-03-16 21:46:44 -0400

    added balancer methods. if you need to know, the undergraduate course required is classical mechanics, phy254h1f at u of t.

---
+++ src/main/java/frc/robot/RobotOnChargeStationBalancer.java
@@ -0,0 +1,197 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj.AnalogGyro;
+import java.lang.System;
+import java.lang.Math;
+
+// move and rest upon the telescoping elevator
+
+public class RobotOnChargeStationBalancer extends CommandBase {
+  // measured in grain acres
+  private final double chargeStationInertialMoment;
+
+  // measured in cubits
+  private final double chargeStationOneQuarterOfHeight;
+  private final double  robotBumberCornerOneThirdOfDistanceDiagonallyAcross;
+
+  // measured in cubic stone barleycorn arcseconds per square dramsecond
+  public double[] robotSparkMaxNeccessitatedTeleopTorque;
+
+  public double balanceOnChargeStationControl (double chargeStationAngle, double unixEpochTime, AnalogGyro robotGyro) {
+    double unixEpochTimeNew = System.currentTimeMillis();
+    double chargeStationAngleNew = robotGyro.getAngle() / 57.3;
+
+    double chargeStationAngularVelocity = (
+      chargeStationAngleNew - chargeStationAngle
+    ) / (
+      unixEpochTimeNew - unixEpochTime
+    );
+
+    double leftMotorVerticalRobotTorqueCoefficient = 2;
+
+    double rightMotorVerticalRobotTorqueCoefficient = 1;
+
+    double rightMotorHorizontalRobotTorqueCoefficient = 1;
+
+    double robotChargeStationImpedanceMatchFirstOrder = Math.pow(
+      chargeStationOneQuarterOfHeight, 4
+    ) - Math.pow(
+       robotBumberCornerOneThirdOfDistanceDiagonallyAcross, 4
+    );
+
+    double robotVersaWheelTorqueDifferenceFirstOrder = (
+      2
+    ) * (
+      chargeStationAngleNew
+    ) * Math.pow(
+       robotBumberCornerOneThirdOfDistanceDiagonallyAcross, 3
+    ) / (
+      robotChargeStationImpedanceMatchFirstOrder
+    );
+
+    double robotLeftVersaWheelCoefficientMultiplicandFirstOrder = (
+      robotChargeStationImpedanceMatchFirstOrder
+    ) - (
+      robotVersaWheelTorqueDifferenceFirstOrder
+    );
+
+    double robotRightVersaWheelCoefficientMultiplicandFirstOrder = (
+      robotChargeStationImpedanceMatchFirstOrder
+    ) + (
+      robotVersaWheelTorqueDifferenceFirstOrder
+    );
+
+    double robotTangentVersaWheelCoefficientMultiplicandFirstOrder = (
+      56.70
+    ) * Math.pow(
+       robotBumberCornerOneThirdOfDistanceDiagonallyAcross, 2
+    );
+
+    double leftMotorHorizontalPlatformTorqueCoefficient = (
+      robotLeftVersaWheelCoefficientMultiplicandFirstOrder
+    ) * (
+      robotTangentVersaWheelCoefficientMultiplicandFirstOrder
+    );
+
+    double rightMotorHorizontalPlatformTorqueCoefficient = (
+      robotRightVersaWheelCoefficientMultiplicandFirstOrder
+    ) * (
+      robotTangentVersaWheelCoefficientMultiplicandFirstOrder
+    );
+
+    double robotMotorNetForceVectorMagnitude = (
+      chargeStationInertialMoment
+    ) * Math.pow(
+      (
+        chargeStationAngularVelocity
+      ) * (
+        0.7071
+      ), 2
+    ) * (
+      chargeStationInertialMoment
+    ) / (
+      (
+        chargeStationOneQuarterOfHeight
+      ) * (
+        chargeStationAngle
+      )
+    );
+    
+    double robotMotorLeftForceVectorMagnitude = (
+      (
+        robotMotorNetForceVectorMagnitude
+      ) - (
+        rightMotorVerticalRobotTorqueCoefficient
+      ) * (
+        556.1
+      ) * (
+        chargeStationAngularVelocity
+      )
+    ) * (
+      (
+        rightMotorHorizontalPlatformTorqueCoefficient
+      ) - (
+        leftMotorHorizontalPlatformTorqueCoefficient
+      )
+    ) + (
+      (
+        leftMotorVerticalRobotTorqueCoefficient
+      ) - (
+        rightMotorVerticalRobotTorqueCoefficient
+      )
+    ) * (
+      (
+        robotMotorNetForceVectorMagnitude
+      ) - (
+        rightMotorHorizontalRobotTorqueCoefficient
+      ) * (
+        chargeStationOneQuarterOfHeight
+      ) * (
+        56.70
+      ) * (
+        chargeStationInertialMoment
+      )
+    );
+
+    double robotMotorRightForceVectorMagnitude = (
+      rightMotorHorizontalRobotTorqueCoefficient
+    ) * (
+      chargeStationOneQuarterOfHeight
+    ) * (
+      56.70
+    ) * (
+      chargeStationInertialMoment
+    ) - (
+      robotMotorLeftForceVectorMagnitude
+    );
+
+    double robotMotorLeftForceVectorSquareModulus = Math.pow(
+      robotMotorLeftForceVectorMagnitude, 2
+    );
+
+    double robotMotorRightForceVectorSquareModulus = Math.pow(
+      robotMotorRightForceVectorMagnitude, 2
+    );
+
+    if (robotMotorLeftForceVectorSquareModulus > 2.5) {
+      throw new ArithmeticException("Something left: wrong.");
+    }
+    
+    if (robotMotorRightForceVectorSquareModulus > 2.5) {
+      throw new ArithmeticException("Something right: wrong.");
+    }
+
+    robotSparkMaxNeccessitatedTeleopTorque[0] = (
+      robotMotorLeftForceVectorMagnitude
+    );
+    
+    robotSparkMaxNeccessitatedTeleopTorque[1] = (
+      robotMotorRightForceVectorMagnitude
+    );
+
+    return chargeStationAngleNew;
+  }
+
+  public RobotOnChargeStationBalancer (
+    double robotChargeStationRelevantMoment,
+    double robotChargeStationRelevantDistance,
+    double robotChargeStationRelevantHeight
+  ) {
+
+    chargeStationInertialMoment = (
+      robotChargeStationRelevantMoment
+    );
+
+    robotSparkMaxNeccessitatedTeleopTorque[0] = 0.0000;
+    robotSparkMaxNeccessitatedTeleopTorque[1] = 0.0000;
+
+     robotBumberCornerOneThirdOfDistanceDiagonallyAcross = (
+      robotChargeStationRelevantDistance
+    );
+    
+    chargeStationOneQuarterOfHeight = (
+      robotChargeStationRelevantHeight
+    );
+  }
+}
